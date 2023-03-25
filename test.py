07397b848751daafcb13b2553e21d18040b6b2fb
Alex <alex@a.com>
2023-03-25 23:52:08 +0300

    Added files to the test

--- test.py
+++ test.py
@@ -63,3 +63,70 @@ def sigh_up(user_data):
 
 sigh_up({'name': 'Alex', 'age': 39})
 sigh_up({'name': 'Alex', 'age': '39'})
+
+
+##############################################
+##############################################
+
+# files 
+# file_message = open('message.txt', 'w')
+# file_message.close()
+
+
+# with open('message.txt', 'w') as file_message:
+#     file_message.write('Hello, world!\n')
+
+
+# with open('message.txt', 'w') as file_message:
+#     file_message.write('Hello!\n')
+
+
+# with open('message.txt', 'a') as file_message:
+#     file_message.write('Goodbye!\n')
+
+
+# with open('message.txt', 'a') as file_message:
+#     file_message.write('Привет!\n')
+
+
+# with open('message.txt', 'a', encoding='utf-8') as file_message:
+#     file_message.write('Привет!\n')
+
+
+# with open('message.txt', 'r', encoding='utf-8') as file_message:
+#     print(file_message.read(10))
+
+
+# with open('message.txt', 'r', encoding='utf-8') as file_message:
+#     print(file_message.read())
+
+
+# with open('message.txt', 'r', encoding='utf-8') as file_message:
+#     print(file_message.readline())
+#     print(file_message.readline())
+
+
+# with open('message.txt', 'r', encoding='utf-8') as file_message:
+#     print(file_message.readlines())
+
+
+# with open('message.txt', 'r', encoding='utf-8') as file_message:
+#     print(next(file_message), end="")
+#     print(next(file_message), end="")
+
+
+import os
+
+# os.mkdir("messages")
+
+# with open('messages/message.txt', 'a', encoding='utf-8') as file_message:
+#     file_message.write('Привет!\n')
+
+# os.rename('messages/message.txt', 'messages/message1.txt')
+
+# os.remove('messages/message1.txt')
+# os.rmdir('messages')
+
+
+##############################################
+##############################################
